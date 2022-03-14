c005b3272e4c957f3a95ffc052bd73a7d815b4f8
sakthivelCardinality <sakthivel.a@cardinality.ai>
2022-03-14 11:15:01 +0530

    added try catch block in the excel code

--- readEXCELfile.js
+++ readEXCELfile.js
@@ -12,13 +12,21 @@ const fs = require('fs');
 // });
 
 var workbook = XLSX.readFile(
-  './Missing_ApplicationPovertyLevel_Production_10052021.xlsx'
+  './AISCasesCreatedOrUpdatedAfterDBRefresh_UAT_03102022.xlsx'
 );
 var sheet_name_list = workbook.SheetNames;
 // console.log(sheet_name_list);
 var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
-fs.unlinkSync('insert_query.sql');
+const fileName = 'insertQuery.sql';
+
+try {
+  if (fs.existsSync(fileName)) {
+    fs.unlinkSync(fileName);
+  }
+} catch (error) {
+  console.error(error);
+}
 
 const baseQuery = `INSERT INTO public.aisschedulerdetails
 (aisschedulerid, casenumber, caseseqnumber, caseupdateddate, status, attempt, errorcode, errormessage, createdate, updateddate)
@@ -32,12 +40,12 @@ try {
 
 xlData.map((value, index) => {
   console.log(index + 1 + ' - ' + xlData.length);
-  let query = `('cfbc2da9-4f87-4b50-b7ab-21a4d478715c'::uuid, '${value.CaseFileGenID}', ${value.ApplicationVersionNumber}, current_timestamp, 'improgress_3', 0, NULL, NULL, NULL, NULL), \n`;
+  let query = `('7029c107-46c3-4975-a6a0-17a44bd373a0'::uuid, '${value.CaseNumber}', ${value.CaseSeqNumber}, current_timestamp, 'improgress_1', 0, NULL, NULL, NULL, NULL), \n`;
   if (parseInt(index + 1) === parseInt(xlData.length)) {
     query = query.replace(/,\s*$/, ';');
   }
   try {
-    fs.appendFileSync('insert_query.sql', query);
+    fs.appendFileSync(fileName, query);
   } catch (error) {
     console.error(error);
   }
